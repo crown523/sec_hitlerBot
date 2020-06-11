70a9b2578b3259d8e59927f2de9719b71944ad9d
crown523 <kxlider@rocketmail.com>
2020-06-11 15:52:27 +0800

    setup, joining game

--- shitler.js
+++ shitler.js
@@ -14,8 +14,36 @@ var bot = new Discord.Client({
 });
 
 // Declaring variables for game setup
-var playerIDs = []; // empty list to be filled with player IDs
+let playerIDs = []; // empty list to be filled with player IDs
+let numPlayers;
+let policyTiles = []; //policy tile deck
+let initiated = false;
+let gameInProgress = false;
 
+
+function init() {
+    policyTiles = ['B','B','B','B','B','B','R','R','R','R','R','R','R','R','R','R','R'];
+    shuffle();
+    playerIDs.length = 0;
+    initiated = true;
+}
+
+function endGame() {
+    initiated = false;
+    gameInProgress = false;
+}
+
+//shuffle tiles
+function shuffle() {
+    console.log("hi");
+    let count = policyTiles.length;
+    while (count) {
+        //remove a random card and put it at the top
+        policyTiles.push(policyTiles.splice(Math.floor(Math.random() * count), 1)[0]); 
+        count--;
+    }
+
+}
 
 bot.on('ready', function (evt) {
     logger.info('Connected');
@@ -38,16 +66,62 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     message: 'austin is a stupidhead'
                 });
             break;
-            case 'start':
-                gameStart();
+            case 'init':
+                if (initiated) {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: `<@${userID}>, a game already exists! Type ~join to join it.`
+                    })
+                } else if (gameInProgress) {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: `<@${userID}>, a game is already in progress, please wait for it to end!`
+                    })
+                } else {
+                    init();
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'Welcome to Secret Hitler! Type ~join if you want to play!'
+                    });
+                    bot.sendMessage({
+                        to: channelID,
+                        message: "(DEBUG) " + policyTiles
+                    });
+                }
             break;
+            case 'join':
+                if (!initiated) {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: `<@${userID}>, no game currently exists. Type ~init to create one!`
+                    })
+                } else if (gameInProgress) {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: `<@${userID}>, a game is already in progress, please wait for it to end!`
+                    })
+                } else if (playerIDs.length < 11) {
+                    playerIDs.push(userID);
+                    bot.sendMessage({
+                        to: channelID,
+                        message: `<@${userID}> has joined the game! There are ${playerIDs.length} player(s) currently. If all players have joined, type ~start to start the game.`
+                    });
+                } else {
+                    bot.sendMessage({
+                        to: channelID,
+                        message: `Sorry <@${userID}>, the game is full. Please wait for the next one!`
+                    });
+                }
+                break;
+            case 'abort':
+                endGame();
+                bot.sendMessage({
+                    to: channelID,
+                    message: `Game Aborted`
+                });
+                break;
             // Just add any case commands if you want to..
          }
      }
 });
-function gameStart() {
-    bot.sendMessage({
-        to: channelID,
-        message: 'Welcome to Secret Hitler! Type ~join if you want to play!'
-    })
-}
+
